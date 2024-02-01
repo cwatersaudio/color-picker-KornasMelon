c2990f9a2d88bf21ca7ffcc84e43def2dd136c1c
Kornasmelon <korsamelson@gmail.com>
2024-02-01 14:48:36 +0545

    added the code

---
+++ index.js
@@ -0,0 +1,95 @@
+// MAIN TASK:
+// get the hex to copy
+
+// side tasks:
+// remember to add the note from the bottom to here to ask about this managing system in the end
+// make notes everywhere
+// add all the modes needed
+
+// make everything responsive (em rem)
+
+document.getElementById('get-color-scheme-btn').addEventListener('click', (e) => {  
+    let seedColorEl = document.getElementById('seed-color').value
+    let seedColor = seedColorEl.replace('#', '')
+
+    // this one took a while xD
+    // gets selectedModeList(the select element thatb holds the options) then gets the value of the selected option. i tried many methods so it was pretty confusing but it ended upp as the simplest one.
+    const selectedModeList = document.getElementById("selected-mode-list")
+    const value = selectedModeList.value
+    
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${value.toLowerCase()}`, 
+        {method: "GET"})
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            const colorsArray = []
+            let index = 0
+            // pushes color values to colorsArray
+            data.colors.forEach(() => {
+                colorsArray.push(data.colors[index].hex.value)
+                index ++
+            })
+            
+            let colorsArrayHTML = ''
+            index = 0
+            colorsArray.forEach(() => {
+                colorsArrayHTML += `
+                    <div
+                        class="color-container" 
+                        style="background-color:${colorsArray[index]}">
+                        <p 
+                            class="hex-color-style" 
+                            id="hex-value" 
+                            onclick="copyToClipboard('${colorsArray[index]}')">${colorsArray[index]}
+                        </p>
+                    </div>
+                `
+                index ++
+            })
+            
+            document.getElementById('colors-container').innerHTML = colorsArrayHTML
+            
+        })
+})
+
+// copies the text when its clicked
+// tried for a long time to figure out how to gget it to copy automatically but kept running into problems and the
+function copyToClipboard(text) {
+    window.prompt("To copy to clipboard, Press Ctrl+C, Enter", text)
+}
+
+
+function myFunction() {
+  // Get the text field
+  const hexValue = document.getElementById("hex-value");
+
+  // Select the text field
+  hexValue.select();
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(hexValue.value);
+  
+  // Alert the copied text
+  alert("Copied the text: " + hexValue.value);
+
+
+
+
+
+
+
+
+
+
+
+
+// MAIN TASK:
+// add the color hex below the color
+
+// side tasks:
+// make notes everywhere
+// add all the modes needed
+// style header
+// make everything responsive (em rem)
+// make everything align centerly, or fit to a compuper
+
